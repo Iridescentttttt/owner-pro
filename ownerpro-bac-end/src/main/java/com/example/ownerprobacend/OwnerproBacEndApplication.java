b6ff755969ad29c1e00f0cc25a01d5988196e703
hdh173 <118664544+hdh173@users.noreply.github.com>
2023-01-26 18:33:38 +0800

    1.26

---
+++ ownerpro-bac-end/src/main/java/com/example/ownerprobacend/OwnerproBacEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.ownerprobacend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerproBacEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerproBacEndApplication.class, args);
+	}
+
+}
---
+++ ownerpro-bac-end/src/test/java/com/example/ownerprobacend/OwnerproBacEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ownerprobacend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerproBacEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
